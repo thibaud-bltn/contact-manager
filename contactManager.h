739e5923b788554871e63a872640c56f39a43510
thibaud-bltn <thibaud.bellaton@edu.ece.fr>
2025-01-18 23:04:14 +0100

    Main menu creation

--- contactManager.h
+++ contactManager.h
@@ -1,7 +1,9 @@
 #ifndef CONTACTMANAGER_CONTACTMANAGER_H
 #define CONTACTMANAGER_CONTACTMANAGER_H
 
+#include <iostream>
 #include <string>
+#include <vector>
 
 struct Contact {
     std::string firstName;
@@ -9,5 +11,29 @@ struct Contact {
     int phoneNumber;
     std::string email;
 };
+
+void printBanner() {
+    std::cout << "   ______            __             __     __  ___                                 " << std::endl
+              << "  / ____/___  ____  / /_____ ______/ /_   /  |/  /___ _____  ____ _____ ____  _____" << std::endl
+              << " / /   / __ \\/ __ \\/ __/ __ `/ ___/ __/  / /|_/ / __ `/ __ \\/ __ `/ __ `/ _ \\/ ___/" << std::endl
+              << "/ /___/ /_/ / / / / /_/ /_/ / /__/ /_   / /  / / /_/ / / / / /_/ / /_/ /  __/ /" << std::endl
+              << "\\____/\\____/_/ /_/\\__/\\__,_/\\___/\\__/  /_/  /_/\\__,_/_/ /_/\\__,_/\\__, /\\___/_/" << std::endl
+              << "                                                                /____/             " << std::endl
+              << std::endl;
+}
+
+void printMenu() {
+    std::cout << std::endl;
+    std::cout << "=========================================" << std::endl;
+    std::cout << "             Contact Manager             " << std::endl;
+    std::cout << "=========================================" << std::endl;
+    std::cout << "1. Add a contact" << std::endl
+              << "2. Edit a contact" << std::endl
+              << "3. Delete a contact" << std::endl
+              << "4. Show contacts" << std::endl
+              << "5. Leave" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+    std::cout << "Select an option: ";
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+#include "contactManager.h"
 
 int main() {
 
+    int choice {-1};
+    std::vector<Contact> contacts;
 
+    printBanner();
 
+    while (choice != 5) {
+
+        printMenu();
+
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                break;
+        }
+    }
+
+    return 0;
 }
